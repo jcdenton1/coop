9f7e96186e795552c03a9f0276a87aff1618aed0
Ilia Lev <jcdenton1@gmail.com>
2017-08-16 19:25:04 +0300

    remove unnecessary files

--- admin/public/js/controllers/GeekCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('GeekCtrl', []).controller('GeekController', function($scope, $http) {
-
-	$scope.tagline = 'The square root of life is pi!';
-
-	$http.get("/geeks").then(function (response) {
-		$scope.geeks = response.data;
-	});
-
-});
--- admin/public/js/controllers/NerdCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope, $http) {
-
-	$scope.tagline = 'Nothing beats a pocket protector!';
-
-	$http.get("/nerds").then(function (response) {
-		$scope.nerds = response.data;
-	});
-
-});
